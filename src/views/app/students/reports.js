9a23fe690087dbc3a360b289e0a75ad011d56259
engAbdullahHanif <hanif@aseelapp.com>
2023-02-09 11:32:23 +0430

    students report updated

--- src/views/app/students/reports.js
+++ src/views/app/students/reports.js
@@ -31,11 +31,14 @@ import {
 const colors = ThemeColors();
 
 const servicePath = 'http://localhost:8000';
-const teachersInstituteApiUrl = `${servicePath}/api/schoole_teachers_province/`;
+const teachersInstituteApiUrl = `${servicePath}/teachers/schoole_teachers_province/`;
 const studentsApiUrl = `${servicePath}/api/province_student_statistics/`;
 const sectorsInstitutesApiUrl = `${servicePath}/institutes/sectors-institutes`; //api not developed
 const sectorsStudentsApiUrl = `${servicePath}/api/sectors-students`; //api not developed
 const classStudentsApiUrl = `${servicePath}/api/class-students`; //api not developed
+const newEnrolledStudentsApiUrl = `${servicePath}/api/new-enrolled-students`; //api not developed
+const teachersApiUrl = `${servicePath}/teachers/province_teacher_count`;
+const instituteBuildingApiUrl = `${servicePath}/institutes/institute_building_count`; //api not developed
 
 const quillModules = {
   toolbar: [
@@ -68,42 +71,101 @@ const quillFormats = [
 const Reports = () => {
   const [textQuillStandart, setTextQuillStandart] = useState('اینجا کابل هست');
   const [isLoaded, setIsLoaded] = useState(false);
+
   const [students, setStudents] = useState([]);
+  const [studentsLoading, setStudentsLoading] = useState(false);
+
   const [teachersInstitute, setTeachersInstitute] = useState([]);
+  const [teachersInstituteLoading, setTeachersInstituteLoading] =
+    useState(false);
+
   const [sectorsInstitutes, setSectorsInstitutes] = useState([]);
+  const [sectorsInstitutesLoading, setSectorsInstitutesLoading] =
+    useState(false);
+
   const [sectorsStudents, setSectorsStudents] = useState([]);
+  const [sectorsStudentsLoading, setSectorsStudentsLoading] = useState(false);
+
+  const [classStudents, setClassStudents] = useState([]);
+  const [classStudentsLoading, setClassStudentsLoading] = useState(false);
+
+  const [newEnrolledStudents, setNewEnrolledStudents] = useState([]);
+  const [newEnrolledStudentsLoading, setNewEnrolledStudentsLoading] =
+    useState(false);
+
+  const [teachers, setTeachers] = useState([]);
+  const [teachersLoading, setTeachersLoading] = useState(false);
+
+  const [instituteBuilding, setInstituteBuilding] = useState([]);
+  const [instituteBuildingLoading, setInstituteBuildingLoading] =
+    useState(false);
 
   const fetchStudents = async () => {
     const response = await axios.get(studentsApiUrl);
     const updatedData = await response.data;
     setStudents(updatedData);
-    setIsLoaded(true);
+    setStudentsLoading(true);
   };
 
   const fetchTeachersInstitute = async () => {
     const response = await axios.get(teachersInstituteApiUrl);
     const updatedData = await response.data;
     setTeachersInstitute(updatedData);
-    setIsLoaded(true);
+    setTeachersInstituteLoading(true);
   };
 
   const fetchSectorsInstitute = async () => {
     const response = await axios.get(sectorsInstitutesApiUrl);
     const updatedData = await response.data;
-    setTeachersInstitute(updatedData);
-    setIsLoaded(true);
+    setSectorsInstitutes(updatedData);
+    setSectorsInstitutesLoading(true);
   };
 
   const fetchsectorStudents = async () => {
     const response = await axios.get(sectorsStudentsApiUrl);
     const updatedData = await response.data;
-    setTeachersInstitute(updatedData);
-    setIsLoaded(true);
+    setSectorsStudents(updatedData);
+    setSectorsStudentsLoading(true);
+  };
+
+  const fetchClassStudents = async () => {
+    const response = await axios.get(classStudentsApiUrl);
+    const updatedData = await response.data;
+    setClassStudents(updatedData);
+    setClassStudentsLoading(true);
+  };
+
+  const fetchNewEnrolledStudents = async () => {
+    const response = await axios.get(newEnrolledStudentsApiUrl);
+    const updatedData = await response.data;
+    setNewEnrolledStudents(updatedData);
+    setNewEnrolledStudentsLoading(true);
+  };
+
+  const fetchTeachers = async () => {
+    const response = await axios.get(teachersApiUrl);
+    const updatedData = await response.data;
+    setTeachers(updatedData);
+    setTeachersLoading(true);
+  };
+
+  const fetchInstituteBuilding = async () => {
+    const response = await axios.get(instituteBuildingApiUrl);
+    const updatedData = await response.data;
+    setInstituteBuilding(updatedData);
+    setInstituteBuildingLoading(true);
   };
 
   useEffect(() => {
     fetchStudents();
     fetchTeachersInstitute();
+    fetchSectorsInstitute();
+    fetchsectorStudents();
+    fetchClassStudents();
+    fetchNewEnrolledStudents();
+    fetchTeachers();
+    fetchInstituteBuilding();
+    setIsLoaded(true);
   }, []);
 
   const studentsReportChartData = {

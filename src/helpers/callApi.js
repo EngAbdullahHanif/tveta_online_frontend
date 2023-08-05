59ef4647dd9782142328bd56ef052024658217e0
sartaj Ahmad <sartajahmad1529@gmail.com>
2023-08-05 08:26:30 +0430

    add proper error handling to callAPi function

--- src/helpers/callApi.js
+++ src/helpers/callApi.js
@@ -12,20 +12,21 @@ message.config({
 import axios from 'axios';
 
 // const servicePath = 'http://172.16.105.244/tveta'; #production mood
-// const servicePath = 'http://172.16.105.108:8000';
+// const servicePath = 'http://0.0.0.0:8000';
+const servicePath = 'http://172.16.105.108:8000';
 // const servicePath = 'https://online.tveta.gov.af:8000';
-const servicePath = 'http://0.0.0.0:8000';
 
 const start_date = '2023-06-01';
 const end_date = '2023-06-30';
 
-//  get the API headers
+//  sets authentication header and content-type
 const getHeaders = (data) => {
   const user = localStorage.getItem('user');
   const access_token = localStorage.getItem('access_token');
-
   if (user && access_token) {
     const headers = { Authorization: `Bearer ${access_token}` };
+
+    // if data includes files, send request as multipart/form-data
     if (data instanceof FormData) {
       console.log('data instanceof FormData', data instanceof FormData);
       headers['Content-Type'] = 'multipart/form-data';
@@ -44,14 +45,10 @@ const callApi = async (
   params = null
 ) => {
   const headers = getHeaders(data);
-  console.log('HEaders: ', headers);
   const url = `${servicePath}/${endpoint}`;
   console.log('DATA in API Call: ' + endpoint, data);
-  if (data instanceof FormData) {
-    console.log('data is instance of formData');
-  }
-
   console.log('the url is', url);
+
   try {
     const response = await axios({
       method,
@@ -60,38 +57,44 @@ const callApi = async (
       data,
       params,
     });
+
     console.log('CALL API Response: on ' + endpoint, response.data);
     return response;
   } catch (error) {
-    if (error.response.status >= 500 && error.response.status < 600) {
+    if (error.response) {
+      // server responded with a non-2xx code
+      switch (error.response.status) {
+        case 404:
+          NotificationManager.error(
+            'an error occured while requesting resource at ' + endpoint,
+            'پیدا نشو/دریافت نشد',
+            5000
+          );
+          break;
+        default:
+          NotificationManager.error(
+            'an error occured while connecting to server at ' + endpoint,
+            error?.response?.status + ': Server Error',
+            5000
+          );
+      }
+      console.error(error);
+    }
+    if (error.request) {
+      // request was made, but no response
       NotificationManager.error(
-        'په سرور کې کوم مشکل رامینځته شوی،‌مهربانی وکړی له مسسول کس سره تماس ونیسی',
-        'سرور ایرور',
-        5000,
-        null,
-        null,
-        'fill'
+        'an error occured on the server-side ' + endpoint,
+        'Server Error: no response received from server',
+        5000
+      );
+    } else {
+      NotificationManager.error(
+        'An error occured while sending data to server' + endpoint,
+        'Client Error: no response received from server',
+        5000
       );
     }
-    NotificationManager.error(
-      'an error occured while connecting to server at ' + endpoint,
-      error?.response?.status + ': Server Error',
-      5000
-    );
-    console.log('Error in API: ', error);
-    if (error.response && error.response.status === 404) {
-      // throw new Error('Resource not found');
-      NotificationManager.warning(
-        'Resource not found',
-        'Error',
-        10000,
-        null,
-        null,
-        ''
-      );
-    }
-    // console.log('error inside callapi: ', error.request);
-    throw error;
+    return false;
   }
 };
 
